69bbeaa5e1c4f39d5e2a17ecb9200fd80ca78879
Sathish Kumar B <sathishsince2004@gmail.com>
2026-06-23 18:45:10 +0530

    Update ProductRepo.java

--- src/main/java/Sathish292004/repo/ProductRepo.java
+++ src/main/java/Sathish292004/repo/ProductRepo.java
@@ -2,8 +2,6 @@ package Sathish292004.repo;
 
 import Sathish292004.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,10 +9,9 @@ import java.util.List;
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
-    @Query("SELECT p FROM Product p WHERE " +
-            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-    List<Product> searchProducts(@Param("keyword") String keyword);
+    List<Product> findByNameContainingIgnoreCaseOrBrandContainingIgnoreCaseOrCategoryContainingIgnoreCase(
+            String name,
+            String brand,
+            String category
+    );
 }
